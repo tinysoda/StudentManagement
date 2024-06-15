12ba9236122ba55198d7ca62d893a1fa96214f65
Valiant <rainguy.shy@gmail.com>
2024-06-16 04:56:29 +0700

    Initial commit

---
+++ src/studentmanagement/FXMLDocumentController.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentmanagement;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Administrator
+ */
+public class FXMLDocumentController implements Initializable {
+    
+
+    @FXML
+    private FontAwesomeIcon btn_close;
+    @FXML
+    private Button btn_login;
+    
+    @FXML
+    private AnchorPane main_form;
+    
+    @FXML
+    private PasswordField tf_password;
+    
+    @FXML
+    private TextField tf_username;
+    
+//    DBUtils
+    private Connection connect;
+    private PreparedStatement preparedSTM;
+    private ResultSet resultS;
+    
+    
+    
+    public void close(){
+        System.exit(0);
+    }
+
+    public void loginAdmin(){
+        String sql="SELECT * FROM admin WHERE username = ? AND password = ?";
+        
+        connect=database.connectDB();
+        
+        try{
+            Alert alert;
+            preparedSTM=connect.prepareStatement(sql);
+            preparedSTM.setString(1,tf_username.getText());
+            preparedSTM.setString(2,tf_password.getText());
+            
+            resultS=preparedSTM.executeQuery();
+//Check empty field
+            if(tf_username.getText().isEmpty()||tf_password.getText().isEmpty()){
+                alert=new Alert(AlertType.ERROR);
+                alert.setTitle("Error message");
+                alert.setHeaderText(null);
+                alert.setContentText("Fill in all the blank");
+                alert.showAndWait();
+            }else{
+                if(resultS.next()){
+//Proceed to dashboard
+                    alert=new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Infomation message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully logged in");
+                    alert.showAndWait();
+
+//Hide the loginForm
+                    btn_login.getScene().getWindow().hide();
+//Linking dashboard
+                    Parent root = FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
+                    Stage stage=new Stage();
+                    Scene scene=new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+                }else {
+                    alert=new Alert(AlertType.ERROR);
+                    alert.setTitle("Error message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Wrong credentials");
+                    alert.showAndWait();
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/studentmanagement/database.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentmanagement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+
+/**
+ *
+ * @author Administrator
+ */
+public class database {
+    public static Connection connectDB(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connect=DriverManager.getConnection("jdbc:mysql://localhost/studentDB", "root", "");
+            return connect;
+        }catch(Exception e){
+            e.printStackTrace();
+            }
+        return null;
+    }
+}
